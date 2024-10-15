5ac94803a48c5e0d7bf432dfa7d9eba11116ff3b
s.kawaguchi <kawaguchi@oh-laser.com>
2024-10-15 11:45:56 +0900

    modified settings about github-actions user etc.

--- init_host.sh
+++ init_host.sh
@@ -1,14 +1,18 @@
 # ホストマシンの初期化
 sudo apt update
 
-# mysql
+# mysqlインストール
 sudo apt install mariadb-server
-# ...
+# TODO:
+# https://www.digitalocean.com/community/tutorials/how-to-install-mariadb-on-ubuntu-20-04-ja
 
 # TODO: dockerインストール
-# ...
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04-ja
 
 # その他必要なディレクトリ、ファイルの作成
-sudo mkdir /hapi/
 sudo mkdir /hapi/traefik/data/ -p
 sudo touch /hapi/traefik/data/acme.json
+
+# github actionsで使用するユーザーの作成
+useradd -G docker github-actions
+chown -R github-actions:github-actions /hapi
